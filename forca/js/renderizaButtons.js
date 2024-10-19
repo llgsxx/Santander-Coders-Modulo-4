4207ae1a0126b897fd913993eee5edf2d83e2d30
Daniel San <danielgdt88@hotmail.com>
2024-10-19 19:47:12 -0300

    refator: modularizo as functions

---
+++ forca/js/renderizaButtons.js
@@ -0,0 +1,35 @@
+const teclado = document.querySelector(".teclado");
+const alfabeto = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export function renderizaButtons() {
+  alfabeto.forEach(
+    (letra) => (teclado.innerHTML += `<button>${letra}</button>`)
+  );
+}
--- forca/main.js
+++ forca/main.js
@@ -1,37 +1,3 @@
-const teclado = document.querySelector(".teclado");
-const alfabeto = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-function renderizaButtons() {
-  alfabeto.forEach(
-    (letra) => (teclado.innerHTML += `<button>${letra}</button>`)
-  );
-}
+import { renderizaButtons } from "./js/renderizaButtons";
 
 renderizaButtons();
